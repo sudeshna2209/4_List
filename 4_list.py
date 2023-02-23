8f2fbb3556875160f37d42c469778797b26661ad
sudeshna2209 <125039157+sudeshna2209@users.noreply.github.com>
2023-02-23 12:56:50 +0530

    Add files via upload

---
+++ 4_list.py
@@ -0,0 +1,28 @@
+# a=[]
+# print(type(a))
+
+# a=["python",1,2.5,[23,43]]
+# print(a,type(a))
+# print(a[0],type(a[0]))
+# print(a[1],type(a[1]))
+# print(a[2],type(a[2]))
+# print(a[3],type(a[3]))
+# print(a[4])  # index error:list index out of range
+
+# a=[1,3.5,"hello",[45,56]]
+# print(len(a))
+
+# a=None
+# print(type(a))
+
+'''Modifications can be done in list where as in strings it is not possible'''
+
+# a=["hello",3.9,7,[67,32]] 
+# a[0]=23
+# a[3][0]=21
+# a[-2]=53
+# print(a)
+
+
+# print(dir(list))
+
